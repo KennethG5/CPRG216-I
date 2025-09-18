913db37baea23ae28782dd4522c1775dce180497
Kenneth Gomez <kennethneil0123@gmail.com>
2025-09-18 16:06:04 -0600

    Add files via upload

---
+++ week3.py
@@ -0,0 +1,17 @@
+# This is our 3rd class demo
+'''  
+in this class we will work on 
+variables and other stuff
+'''
+# Review
+# Assignment
+
+a = 3
+b = 4.5
+c = True
+msg = 'hello world!' 
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(msg, type(msg))
